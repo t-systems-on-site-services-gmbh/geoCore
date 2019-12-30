14a4c7eea4a5d91c253ea58232c27c80f15422b4
Gerrit Bette <gerrit.bette@t-systems.com>
2019-12-30 15:49:50 +0100

    + prepare data for drawing

--- profileBuilder.py
+++ profileBuilder.py
@@ -1,6 +1,7 @@
 """Module to construct profiles"""
 
-from qgis.core import Qgis, QgsExpression, QgsFeatureRequest, QgsProject
+import re
+from qgis.core import Qgis, QgsExpression, QgsFeatureRequest, QgsProject, QgsMessageLog
 
 from .geoCoreConfig import Config
 
@@ -11,15 +12,10 @@ class ProfileBuilder:
         """Features are the 'Stammdaten', i.e. data regarding the drilling profiles.
         config is the configuration element containing metadata to profiles."""
         self.showMessage = showMessage
+        self.petroPattern = re.compile(r"(\w+)\s*(\(.*\))?", re.IGNORECASE)
         self.config = Config(self.showErrorMessage)
 
-    def isFeatureAdmissible(self, feature):
-        """Checks if feature is admissible,
-        i.e. do we have "Schichtdaten" for it."""
-        profileId = feature.attribute("id")
-        return self.getSchichtdaten(profileId) is not None
-
-    def getSchichtdaten(self, profileId):
+    def _getSchichtdaten(self, profileId):
         """Get the Schichtdaten corresponding to given drilling profile"""
         # As of now we need to chop off the leading letter.
         # This might change in the future.
@@ -33,10 +29,33 @@ class ProfileBuilder:
 
         return layerSchichtdaten[0].getFeatures(qfr)
 
+    def _getLayerAttributes(self, schichtdaten):
+        """Get a list of dictionaries containing the layers' attributes"""
+        result = []
+        for sd in schichtdaten:
+            result.append({field.name(): attr for field, attr in zip(sd.fields(), sd.attributes())})
+        return result
+
+    def _splitPetrographie(self, petro):
+        """Split the given Petrograhie into Großgruppe and Kleingruppe"""
+        m = self.petroPattern.match(petro)
+        gg = m.group(1) # Großgruppe
+        kg = []
+        k = m.group(2) # Kleingruppe
+        if k is not None:
+            # chop off parantheses
+            k = k[1:-1]
+            # and make a list of it
+            k = k.split(",")
+            for s in k:
+                if not (s.isspace() or (s == '')):
+                    kg.append(s)
+        return (gg, kg)
+
     def drawProfiles(self, features):
         """Construct the drilling profiles"""
         profiles = [self._drawProfile(f) for f in features]
-        
+
         actualProfiles = []
         actualFeatures = []
         for p, f in zip(profiles, features):
@@ -50,8 +69,19 @@ class ProfileBuilder:
 
     def _drawProfile(self, feature):
         """Construct a profile from feature"""
-        if self.isFeatureAdmissible(feature):
+        profileId = feature.attribute("id")
+        schichtdaten = self._getSchichtdaten(profileId)
+        if schichtdaten is None:
             return None
+
+        boxes = self.config.geoCore['boxes']
+        layerAttributes = self._getLayerAttributes(schichtdaten)
+        for l in layerAttributes:
+            gg, kg = self._splitPetrographie(l['petrographie'])
+            height = l['tiefe bis']-l['tiefe von']
+            width = boxes[gg]['width']
+            QgsMessageLog.logMessage("Profile {} - petro: {}({}), width: {}, height: {}".format(profileId, gg, kg, width, height), level=Qgis.Info)
+
         return True
 
     def _connectProfiles(self, profiles, features):
