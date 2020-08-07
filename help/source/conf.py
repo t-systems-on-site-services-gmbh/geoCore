4648c2c036fa354b5ca8ebbcf327729ff3eac280
Bette, Gerrit Utz <gerrit.bette@t-systems.com>
2020-08-07 13:03:26 +0200

    o license

--- help/source/conf.py
+++ help/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# PetroProfile documentation build configuration file, created by
+# geoCore documentation build configuration file, created by
 # sphinx-quickstart on Sun Feb 12 17:11:03 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
--- profileBox.py
+++ profileBox.py
@@ -1,4 +1,23 @@
-"""Module containing the class ProfileBox"""
+""" Module containing the class ProfileBox
+
+    geoCore - a QGIS plugin for drawing drilling profiles
+    Copyright (C) 2019, 2020  Gerrit Bette, T-Systems on site services GmbH
+
+    This file is part of geoCore.
+
+    geoCore is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    geoCore is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with geoCore.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 #from qgis.PyQt import QtWidgets
 from qgis.PyQt.QtGui import QBrush, QColor, QPen
